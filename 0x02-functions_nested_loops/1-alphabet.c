058904b75919af3e7cbe4dfe73011579e886772e
mathewos <mathewos1@yahoo.com>
2023-02-21 15:00:07 -0800

    Q1&2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - PRINT SMALL LETER ALPHABET
+ *
+ * Return Always 0.
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
