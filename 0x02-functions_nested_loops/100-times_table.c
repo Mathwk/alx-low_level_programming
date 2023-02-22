e1d69d5fa1da993f24cb0e802acffcdd1c2b3e7c
mathewos <mathewos1@yahoo.com>
2023-02-22 02:47:57 -0800

    Q12 correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,8 +20,8 @@ void print_times_table(int n)
 				if (i * j > 99)
 				{
 					_putchar('0' + (i * j) / 100);
-					_putchar('0' + ((i * j) % 100 ) /10);
-					_putchar('0' + ((i + j) % 100) % 10);
+					_putchar('0' + (i * j) / 10);
+					_putchar('0' + (i + j) % 10);
 				}
 				else if (i * j > 9 && i * j < 100)
 				{
