ea2340b17064401240ebfe77fd72b2eacb82869e
mathewos <mathewos1@yahoo.com>
2023-02-21 19:08:30 -0800

    Q11 correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,11 +9,12 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 		for (; n <= 98; n++)
 			printf("%d, ", n);
-	else
+	else if (n > 98)
 		for (; n > 98; n--)
 			printf("%d, ", n);
+	printf("%d", n);
 	printf("\n");
 }
