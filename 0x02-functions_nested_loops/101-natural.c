1ecf075c47ddedcbfa7f3ea259d4dc7480e18413
mathewos <mathewos1@yahoo.com>
2023-02-22 03:15:49 -0800

    Q13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the sum of all the multiples of 3 and 5 below 1024
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int sum;
+
+	n = 1024;
+	sum = 0;
+	for (; n > 0; n--)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+		else if (n % 15 == 0)
+			sum -= n;
+	}
+}
