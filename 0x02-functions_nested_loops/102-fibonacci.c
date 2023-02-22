b4bc71770bbc290e821a766ad5121b06eb84e98e
mathewos <mathewos1@yahoo.com>
2023-02-22 05:26:01 -0800

    Q14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - the first 50 fibonacci that start with 1 and 2
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int c;
+
+	i = 1;
+	j = 2;
+
+	for (c = 0; c < 50;)
+	{
+			printf("%d, ", i);
+			c++;
+			printf("%d, ", j);
+			c++;
+			i += j;
+			j += i;
+	}
+	printf("\n");
+	return (0);
+}
