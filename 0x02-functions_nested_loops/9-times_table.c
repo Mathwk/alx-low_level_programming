215f0685464a14b17c98ef41c4fc24e484290a0d
mathewos <mathewos1@yahoo.com>
2023-02-21 18:00:40 -0800

    Q9 correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,8 +13,16 @@ void times_table(void)
 	{
 		for (j = 0; j <10; j++)
 		{
-			_putchar('0' + (i * j));
-			_putchar(',');
+			if (i * j > 9)
+			{
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
+			}
+			else
+			{
+				_putchar('0' + (i * j));
+				_putchar(',');
+			}
 			if (j < 9)
 				_putchar(' ');
 		}
