747baf4d127d5cd88e83a1884ad4c80afe60ed73
mathewos <mathewos1@yahoo.com>
2023-02-23 13:31:58 -0800

    Q6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_line - draw a line
+ *@n: length of line
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	for (; n > 0; n--)
+		_putchar(' ';
+	_putchar('\n');
+}
