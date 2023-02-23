483663532189bf3c3bdce415a310a6cf6eb64bb5
mathewos <mathewos1@yahoo.com>
2023-02-23 13:00:47 -0800

    Q5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_numbers - print numbers from 0 to 9
+ * more_numbers - print numbers from 0 to 14, 10 times
  *
  * Return: Always 0
  */
-void print_numbers(void)
+void more_numbers(void)
 {
 	int i;
 	int j;
