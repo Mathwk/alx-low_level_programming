924812e53cc9b05a139d322ae92eb49a5827a99f
mathewos <mathewos1@yahoo.com>
2023-02-23 10:02:18 -0800

    Q1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - checks for upper case letters
+ * @c: letter input
+ * Return: 1 for upper case and 0 otherwise
+ */
+int _ist(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
