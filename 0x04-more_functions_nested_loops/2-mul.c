68f750edb3110c9920a673d88468febc80863712
mathewos <mathewos1@yahoo.com>
2023-02-23 11:55:00 -0800

    Q2 correction

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -7,22 +7,5 @@
  */
 int mul(int a, int b)
 {
-	int r;
-	int c;
-	int i;
-
-	c = 0;
-	r = a * b;
-	for (i = r; i > 0;)
-	{
-		c++;
-		i /= 10;
-	}
-	for (; c >= 0; c--)
-	{
-		i = r / 10 ^ c;
-		r %= 10 ^ c;
-		_putchar('0' + i);
-	}
-	return (0);
+	return (a * b);
 }
