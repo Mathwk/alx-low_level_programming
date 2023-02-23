ababe44cf0dfc4b32a7e0eb042aa09bcb079db4f
mathewos <mathewos1@yahoo.com>
2023-02-23 11:07:41 -0800

    Q2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * mul - multiplication
+ * @a: input1
+ * @b: input2
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int r;
+	int c;
+	int i;
+
+	c = 0;
+	r = a * b;
+	for (i = r; i > 0;)
+	{
+		c++;
+		i /= 10;
+	}
+	for (; c >= 0; c--)
+	{
+		i = r / 10 ** c;
+		r %= 10 ** c;
+		_putchar('0' + i);
+	}
+	return (0);
+}
