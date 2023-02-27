0e8cf1b492444919639e953189b45002144c9740
mathewos <mathewos1@yahoo.com>
2023-02-27 13:29:10 -0800

    Q8correction

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; a[i] != '\0';)
 		i++;
-	for (j = 0; j < n || j < i; j++)
+	for (j = 0; (j < n || j < i) && (j >= 0); j++)
 	{
 		printf("%d", a[j]);
 		if (j == n - 1 || j == i - 1)
