c61a2b5461bcf6d899885e523e29d1471fe5425a
mathewos <mathewos1@yahoo.com>
2023-02-27 09:42:10 -0800

    Q4 mod

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts - prints a string
- * @str: string to be printed
+ * _puts - prints a string in reverse
+ * @s: string to be printed
  * Return: Null
  */
-void _puts(char *str)
+void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0';)
+	for (i = 0; s[i] != '\0';)
 		i++;
 	for (; i > 0; i++)
-		_putchar(str[j]);
+		_putchar(s[i]);
 	_putchar('\n');
 }
