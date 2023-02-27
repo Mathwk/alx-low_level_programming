ad5f5e70c8e8b7d3d9e9c5d10e80274f54dbcb65
mathewos <mathewos1@yahoo.com>
2023-02-27 09:46:53 -0800

    Q4 correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts - prints a string in reverse
+ * print_rev - prints a string in reverse
  * @s: string to be printed
  * Return: Null
  */
@@ -11,6 +11,6 @@ void print_rev(char *s)
 	for (i = 0; s[i] != '\0';)
 		i++;
 	for (; i > 0; i--)
-		_putchar(s[i]);
+		_putchar(s[i - 1]);
 	_putchar('\n');
 }
