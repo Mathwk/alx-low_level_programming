bb375b05db845637001d1548c177ccebe7e20f31
mathewos <mathewos1@yahoo.com>
2023-02-27 08:18:00 -0800

    Q1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two numbers
+ * @a: input 1
+ * @b: input 2
+ * Return: Null
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*a = c;
+	*a = *b;
+	*b = c;
+}
+
