94274b7769ca9741bd51d86fba757998155886f3
mathewos <mathewos1@yahoo.com>
2023-02-27 09:07:34 -0800

    Q2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - count total string length
+ * @s: string to be counted
+ * Return: count
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; z[i] != '\0';)
+		i++;
+	return (i);
+}
