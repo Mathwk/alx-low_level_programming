3aa27cde80265fe5b017d7ffbb3e5c878b13805c
mathewos <mathewos1@yahoo.com>
2023-02-27 09:57:26 -0800

    Q5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - prints a string in reverse
+ * @s: string to be printed
+ * Return: Null
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char *r;
+
+	for (i = 0; s[i] != '\0';)
+		i++;
+	j = 0;
+	for (; i > 0; i--)
+	{
+		r[j] = (s[i - 1]);
+		j++;
+	}
+	s = r;
+}
