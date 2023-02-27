321379c5ca5bc7b07ba618c06381a016edb37bad
mathewos <mathewos1@yahoo.com>
2023-02-27 14:41:18 -0800

    Q10

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcpy - copying
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = src;
+	return (dest);
+}
