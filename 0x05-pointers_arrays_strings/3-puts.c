f89efe46dbe657eaebfec6994f6b70147250cf77
mathewos <mathewos1@yahoo.com>
2023-02-27 09:18:13 -0800

    Q3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ * Return: Null
+ */
+void _puts(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0';)
+		i++;
+	j = i;
+	for (i = 0; i < j; i--)
+		_putchar(s[i]);
+}
