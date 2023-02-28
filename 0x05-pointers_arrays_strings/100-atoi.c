6fe4a54e4a08fe203b4c23251d23dd7377b78692
mathewos <mathewos1@yahoo.com>
2023-02-28 12:10:44 -0800

    Q10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,20 +6,32 @@
  */
 int _atoi(char *s)
 {
-	int a, i, j;
+	int a, b, i, j, k;
+	int r;
 
 	j = 0;
 
-	for (i = 0; s[i] != [[:digit:]])
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] >= '0' && s[i] <= '9')
+			break;
 		if (s[i] == '-')
 			j++;
-		i++;
 	}
-	for (a = i; s[i] == [[:digit:]];)
+
+	for (a = i; s[a] >= '0' && s[a] <= '9';)
 		a++;
-	a = a - 1;
-	strncpy(r1 + i, s, a);
-	if (j % 2 == 0)
-		s *= -1;
-	return (s);
+
+	r = 0;
+	for (; i < a; i++)
+	{
+		k = i;
+		b = 1;
+		for (; (k + 1) < a; k++)
+			b *= 10;
+		r += b * (s[i] - 48);
+	}
+	if (j % 2 != 0)
+		r *= -1;
+	return (r);
+}
