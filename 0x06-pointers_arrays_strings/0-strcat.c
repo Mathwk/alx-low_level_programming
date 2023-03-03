f71a1700aaac36ceb70a8ded4212bc3e4dee5239
mathewos <mathewos1@yahoo.com>
2023-03-03 09:45:15 -0800

    Q0 correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j, n;
+	int i, j;
 
 	for (i = 0; dest[i] != '\0';)
 		i++;
 	for (j = 0; src[j] != '\0';)
 		j++;
-	n = j;
-	for (j = 0; j >= n; j++)
+	for (; j >= 0; j--)
 		dest[i + j] = src[j];
 	return (dest);
 }
