1a36f26d0e72933805860147f60366ae1fc173ad
mathewos <mathewos1@yahoo.com>
2023-03-03 14:19:06 -0800

    Q5 mod

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * string_toupper - change all to upper case
- * @s: string to be changed
+ * @str: string to be changed
  * Return: string
  */
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
 	for (i = 0;; i++)
 	{
-		if (s[i] == '\0')
+		if (str[i] == '\0')
 			break;
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= ('a' - 'A');
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= ('a' - 'A');
 	}
-	return (s);
+	return (str);
 }
