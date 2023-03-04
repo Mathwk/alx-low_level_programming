8c7650aea7715d882b6b6b5f2bbb659c86bf1d51
mathewos <mathewos1@yahoo.com>
2023-03-03 16:24:22 -0800

    Q7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - encodes astring into 1337
+ * @s: string to be encoded
+ * Return: string
+ */
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+		i++;
+	for (; i > 0; i--)
+	{
+		if (s[i - 1] == 'a' || s[i - 1] == 'A')
+			s[i - 1] = 4;
+		else if (s[i - 1] == 'e' || s[i - 1] == 'E')
+			s[i - 1] = 3;
+		else if (s[i - 1] == 'o' || s[i - 1] == 'O')
+			s[i - 1] = 0;
+		else if (s[i - 1] == 't' || s[i - 1] == 'T')
+			s[i - 1] = 7;
+		else if (s[i - 1] == 'l' || s[i - 1] == 'L')
+			s[i - 1] = 4;
+	}
+	return (s);
+}
