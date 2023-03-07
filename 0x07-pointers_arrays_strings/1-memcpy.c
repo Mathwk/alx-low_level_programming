34d29eec8681dcddf6f276a10bb3d0dacd022079
mathewos <mathewos1@yahoo.com>
2023-03-06 16:18:49 -0800

    Q2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _memcpy - replace with constant character
+ * _memcpy - replace with memory from another file
  * @dest: string to be edited
  * @src: a source file to be add
  * @n: amount of b to be repeated
- * Return: s
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates character
+ * @s: string to be checked
+ * @c: a character to be searched
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != c;)
+		i++;
+	j = i;
+	for (; s[i]; i++)
+		s[i - j] = s[i]
+	return (s);
+}
