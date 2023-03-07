1e57ad57ce94b4bd6e466610ab73f13aa069de37
mathewos <mathewos1@yahoo.com>
2023-03-07 15:58:37 -0800

    Q4 correction

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * reverse_array - reversing the array
  * @a: an array of integers
@@ -8,33 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, s;
+	int i, j;
 
-	i = n;
-	j = 1;
-	while (i > 0)
+	i = n - 1;
+	for (; i >= n / 2; i--)
 	{
-		if (i != n)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		s = a[i - 1];
-		for (; s / 10 > 0;)
-		{
-			s /= 10;
-			j *= 10;
-		}
-		s = a[i - 1];
-		for (; j >= 1; )
-		{
-			putchar('0' + s / j);
-			s %= j;
-			if (j < 10)
-				break;
-			j /= 10;
-		}
-		i--;
+		j = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = j;
 	}
-	putchar('\n');
 }
