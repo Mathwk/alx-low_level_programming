852a06e908f8f997169f747d1f86283cbc8a98cd
mathewos <mathewos1@yahoo.com>
2023-03-06 16:05:06 -0800

    Q1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - replace with constant character
+ * @dest: string to be edited
+ * @src: a source file to be add
+ * @n: amount of b to be repeated
+ * Return: s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = src[i];
+	return (s);
+}
