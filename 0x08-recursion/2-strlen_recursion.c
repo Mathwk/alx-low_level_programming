bccad44ad11f07713cea97fe54f62c94b1d00382
mathewos <mathewos1@yahoo.com>
2023-03-08 10:56:56 -0800

    Q2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints string
+ * @s:string to be printed
+ * Return: count
+ */
+int _strlen_recursion(char *s)
+{
+	static int a = 1;
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return (a - 1);
+	}
+	a++;
+	_strlen_recursion(s + 1);
+}
