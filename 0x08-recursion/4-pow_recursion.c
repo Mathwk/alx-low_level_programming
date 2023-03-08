751778799c7559c25678f623f78d989aa545f364
mathewos <mathewos1@yahoo.com>
2023-03-08 11:50:10 -0800

    Q4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - calculate x the power of y
+ * @x: base number
+ * @y: power
+ * Return: Result
+ */
+int _pow_recursion(int x, int y);
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
