c6db6f7fc825f2157e1b4c1723a0197e3c302318
mathewos <mathewos1@yahoo.com>
2023-03-08 16:22:44 -0800

    Q4

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_palindrome - string symetry checker
+ * @s: input strig
+ * Return: 1 if true 0 other wise
+ */
+int is_palindrome(char *s)
+{       
+        int a;
+        
+        a = str_count(0, s);
+        if (
