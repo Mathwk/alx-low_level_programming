9e2fc278e54542aed30ddc8e42cd4ba995cd8ab7
mathewos <mathewos1@yahoo.com>
2023-03-09 11:34:47 -0800

    Q8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * wildcmp - string comparism
+ * @s1: string to check
+ * @s2: reference checker
+ * Return: 1 if true 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 != *s2 && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	else if (*s1 != *s2 && *s2 != '*')
+	{
+		if (*s1 == '\0' || *(s2 - 1) != '*')
+			return (0);
+		else
+			return (wildcmp(s1 + 1, s2));
+	}
+	return (wildcmp(s1 + 1, s2 + 1));
+}
