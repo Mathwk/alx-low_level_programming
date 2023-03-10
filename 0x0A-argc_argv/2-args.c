740815efa36bcf10413465900af828cb5e8b0b42
mathewos <mathewos1@yahoo.com>
2023-03-10 10:01:33 -0800

    Q2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: string arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc >= 0)
+	{
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+	return (0);
+}
