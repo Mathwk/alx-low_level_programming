e886413c86044e5fd5e66c7c90d4869a68cfc80e
mathewos <mathewos1@yahoo.com>
2023-03-10 15:18:05 -0800

    Q4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: string arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc > 0)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (isdigit(*argv[i]))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+	return (0);
+}
