e21ee632db6067c31350e285061ca3f0984cf64d
mathewos <mathewos1@yahoo.com>
2023-03-10 16:31:51 -0800

    Q4 mod

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(*argv[i]))
+			if (isdigit(*argv[i]) && atoi(argv[i]) >= 0)
 			{
 				sum += atoi(argv[i]);
 			}
