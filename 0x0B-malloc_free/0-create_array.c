3d55bb9a821a357303dcc635fa068f7aaeb877d5
mathewos <mathewos1@yahoo.com>
2023-03-13 06:50:21 -0700

    Q0 and file loading

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * create_array - replace initial and create an array
+ * @size: base string to be edited
+ * @c: character to replace the initial character of size
+ * Return: a pointer or Null
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+
+	s = malloc(size);
+	if (*s == 0)
+		return (NULL);
+	s[0] = c;
+	return (s);
+}
