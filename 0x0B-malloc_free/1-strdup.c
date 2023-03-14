b7c28086e6cc68244b44ff16cf2a2e783578635e
mathewos <mathewos1@yahoo.com>
2023-03-14 06:30:07 -0700

    Q1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,14 +11,15 @@ char *_strdup(char *str)
 	char *s;
 
 	i = 0;
-	while (*str != '\0')
+	while (str[i] != '\0')
 		i++;
 	if (i == 0)
 		return ('\0');
-	s = malloc(i * sizeof(*s));
+	s = malloc((i * sizeof(*s) + 1);
 	if (s == NULL)
 		return ('\0');
 	for (; i > 0; i--)
 		s[i - 1] = str[i - 1];
+	s[i] = '\0';
 	return (s);
 }
