3e6b3d89de71791d876655381a4120137a06550d
mathewos <mathewos1@yahoo.com>
2023-03-14 17:11:07 -0700

    Q5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates arguments
+ * @ac: arg count
+ * @av: args
+ * Return: pointer to new string or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int a, b;
+
+	if (ac == 0 || av == NULL)
+		return ('\0');
+	s = malloc(sizeof(char *));
+	for (a = 0; a < ac; a++)
+	{
+		s[a] = malloc(sizeof(char) * ac);
+		if (s[a] == NULL)
+		{
+			for (b = a; b >= 0; b--)
+				free(s[a]);
+			free(a);
+			return ('\0');
+		}
+		return ('\0');
+	}
+	for (b = 0; b < 1; b++)
+	{
+		for (a = 0; a < ac; a++)
+			s[b][a] = av[a];
+	}
+	return (s);
+}
