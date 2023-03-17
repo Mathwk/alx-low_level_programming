a07ceac34ba3bf791b812c79e000c77320e60f54
mathewos <mathewos1@yahoo.com>
2023-03-16 18:11:41 -0700

    Q4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,17 +12,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	char *s;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
-		return (ptr);
 	s = malloc(new_size);
 	if (s == NULL)
 		return (NULL);
-	else if (new_size != old_size)
+	if (new_size != old_size)
 	{
 		for (i = 0; i < old_size && i < new_size; i++)
 			s[i] = ((char *) ptr)[i];
