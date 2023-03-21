18fd743f9ef9410ea93445e9c4379c5446a27d47
mathewos <mathewos1@yahoo.com>
2023-03-20 17:45:04 -0700

    Q4 structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
 
-	n_dog = malloc(sizeof(dof_t));
+	n_dog = malloc(sizeof(dog_t));
 	if (n_dog == NULL)
 		return (NULL);
 	n_dog->name = name;
