24b8cd67d0de34962ce4b0dfc2003dcd4d826f13
mathewos <mathewos1@yahoo.com>
2023-03-22 09:30:02 -0700

    Q0 Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - print name
  * @name: name to print
@@ -9,5 +10,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
