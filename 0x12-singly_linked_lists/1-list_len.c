5d924f3a7d5440dfeb30a2c1d876e84046c97531
mathewos <mathewos1@yahoo.com>
2023-03-30 13:27:38 -0700

    linked list Q1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * list_len - print content of linked list
+ * @h: linked list input
+ * Return: number node
+ */
+size_t list_len(const list_t *h)
+{
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
