7dae934e8f9395b1b4919b5f70e0b5606a11c05a
mathewos <mathewos1@yahoo.com>
2023-03-30 16:22:06 -0700

    linkind llist Q2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node - add a new node
+ * @head: base node
+ * @str: new string to be added
+ * Return: the new address
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i = 0;
+	list_t *temp;
+
+	if (str == Null)
+		return (NULL);
+	while (str[i])
+		i++;
+	temp = malloc(sizeof(list_t));
+	temp->str = strdup(str);
+	temp->len = i;
+	temp->next = *head;
+	*head = temp;
+
+	return (head);
+}
