bbad1386858824fbc700ee541f1d2ceea2b25120
mathewos <mathewos1@yahoo.com>
2023-03-30 13:02:55 -0700

    linked list Q0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,30 +10,28 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		h = h.next;
-		count++;
 		_putchar('[');
-		if (h.str == NULL)
+		if (h->str == NULL)
 			_putchar('0');
 		else
-			_putchar(h.len);
+			_putchar(h->len + '0');
 		_putchar(']');
 		_putchar(' ');
-		if (h.str == NULL)
+		if (h->str == NULL)
 		{
 			_putchar('(');
 			_putchar('n');
 			_putchar('i');
 			_putchar('l');
 			_putchar(')');
-			_putchar('\n');
-			break;
 		}
 		else
 		{
-			for (i = 0; h.str[i]; i++)
-				_putchar(h.str[i]);
+			for (i = 0; h->str[i]; i++)
+				_putchar(h->str[i]);
 		}
+		h = h->next;
+		count++;
 		_putchar('\n');
 	}
 	return (count);
