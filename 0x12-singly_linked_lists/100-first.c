d175c4b64404a9e36b2d5ca83a1ef380bb61b87e
mathewos <mathewos1@yahoo.com>
2023-03-30 19:02:44 -0700

    attribute to print before excution of main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void __attribute__((constructor)) _printf();
+/**
+ * _printf - prints before main function
+ */
+void _printf(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
