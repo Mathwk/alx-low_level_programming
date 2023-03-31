9d3fa63a00ced676833da280c8a107ab73e1fb44
mathewos <mathewos1@yahoo.com>
2023-03-30 17:21:50 -0700

    linked list Q3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * add_node_end - add a new node at the end
+ * @head: base node
+ * @str: new string to be added
+ * Return: the new address
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int i = 0;
+	list_t *new, *last;
+
+	while (str[i])
+		i++;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	if (str == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+	new->str = strdup(str);
+	new->len = i;
+	head->next = *new;
+
+	return (temp);
+}
