d0f9ff89b9dc20570efe2dae8964d8be41d88d89
mathewos <mathewos1@yahoo.com>
2023-04-04 20:17:07 -0700

    more linked list Q9 correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else
 	{
 		for (count = 1; count < idx; count++)
+		{
+			if ((*head)->next == NULL)
+				return (NULL);
 			*head = (*head)->next;
+		}
 		new_node->next = (*head)->next;
 		(*head)->next = new_node;
 	}
