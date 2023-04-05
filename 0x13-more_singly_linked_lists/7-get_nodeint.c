37cd7e737238d774edf9f8ac988f948ce1a511eb
mathewos <mathewos1@yahoo.com>
2023-04-04 19:51:16 -0700

    more linked list Q7 correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,6 +9,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count;
 
+	if (head == NULL)
+		return (NULL);
 	for (count = 0; count < index; count++)
 		head = head->next;
 	return (head);
