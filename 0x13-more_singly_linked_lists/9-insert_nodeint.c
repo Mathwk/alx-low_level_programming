4f62d5c148785c02595832b32ffa4c04054837d6
mathewos <mathewos1@yahoo.com>
2023-04-05 15:23:01 -0700

    more linked list Q9 continous feed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,14 +20,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	temp = *head;
 	if (idx == 0)
 	{
-		new_node->next = temp;
-		temp = new_node;
+		new_node->next = *head;
+		*head = new_node;
 	}
 	else
 	{
+		temp = *head;
 		for (count = 1; count < idx; count++)
 		{
 			if (temp->next == NULL)
@@ -37,5 +37,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = temp->next;
 		temp->next = new_node;
 	}
-	return (*head);
+	return (new_node);
 }
