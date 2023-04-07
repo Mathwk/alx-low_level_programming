0c0032208190f6d8b262d51e8c99bb0508c1bb88
mathewos <mathewos1@yahoo.com>
2023-04-06 19:43:13 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,28 +7,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int m = 1;
-	unsigned int i;
-	unsigned long int num;
-
-	if (index > 63)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	num = *n;
-	for (i = 0; num > 0; i++)
-	{
-		if (i == index)
-		{
-			if (num % 2 == 1)
-			{
-				for (; i > 0; i--)
-					m *= 2;
-				*n -= m;
-				return (1);
-			}
-			else
-				return (-1);
-		}
-		num /= 2;
-	}
-	return (-1);
+	*n &= ~(1 << index);
+	return (1);
 }
