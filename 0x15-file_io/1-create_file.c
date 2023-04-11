d988fe74cad327782bfd1fc7d8b2619a0de910bc
mathewos <mathewos1@yahoo.com>
2023-04-10 18:20:09 -0700

    system call creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (op == -1)
 		return (-1);
 	count = 0;
-	while (text_content[count])
-		count++;
+	if (text_content != NULL)
+	{
+		while (text_content[count])
+			count++;
+	}
 	wr = write(op, text_content, count);
 	if (wr == -1)
 		return (-1);
