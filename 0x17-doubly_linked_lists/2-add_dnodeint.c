e474d48cb5aac46327ae83f563c6906d5fb25e50
mathewos <mathewos1@yahoo.com>
2023-05-18 12:33:13 -0700

    doubly lined list excercise

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
-	newnode->next = *head;
+	if (*head != NULL)
+		newnode->next = *head;
+	else
+		newnode->next = NULL;
 	newnode->prev = NULL;
 	*head = newnode;
 	return (*head);
