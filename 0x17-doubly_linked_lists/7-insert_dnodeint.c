04847a22f4901a91c3c225659a7f1fe1da80b268
mathewos <mathewos1@yahoo.com>
2023-05-18 16:30:42 -0700

    doubly linked list Q7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (i = 0; i < idx - 1; i++)
 	{
 		temp = temp->next;
-		if (temp->next == NULL)
+		if (temp->next == NULL && i == idx - 1)
 		{
 			newnode = add_dnodeint_end(h, n);
 			return (newnode);
