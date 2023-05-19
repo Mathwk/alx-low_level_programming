582114794eb74aca22eec6560f06e0399ccf2428
mathewos <mathewos1@yahoo.com>
2023-05-18 17:44:09 -0700

    0x017 excerices

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-	if (temp->next == NULL)
+	if (temp == NULL)
 	{
 		newnode = add_dnodeint_end(h, n);
 		return (newnode);
