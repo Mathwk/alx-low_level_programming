24fc053a55ac48767a46efde6f9e1410d3517c46
mathewos <mathewos1@yahoo.com>
2023-05-18 17:25:28 -0700

    0x017 excerices

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - keygen generator
+ * @argc: number of the arguments
+ * @argv: arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		printf("%s", argv[1]);
+	return (0);
+}
