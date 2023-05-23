72007e292b005603b979ac969a535518b15445c4
mathewos <mathewos1@yahoo.com>
2023-05-22 19:14:41 -0700

    Giga million winner exercise

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -O /tmp/lotto.so /tmp https://github.com/Mathwk/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lotto.so
+export LD_PRELOAD=/tmp/lotto.so
